cabd804f5c2e5b799ea8b1fd1c248c7ff0ec977d
user <user@ASEN5254.myguest.virtualbox.org>
2023-09-13 10:39:29 -0600

    I think I have finihsed teh collision detector... needs checking

--- ws/MyBugAlgorithm.cpp
+++ ws/MyBugAlgorithm.cpp
@@ -1,8 +1,24 @@
 #include "MyBugAlgorithm.h"
 
-// returns true if val is between the values specified by x1 and x2
-bool isBetween(val,x1,x2){
+// returns true if val is between the values specified by x1 and x2 exlusive
+bool isBetwOpen(val,x1,x2){
     return (val>std::min(x1,x2)) && (val<std::max(x1,x2));
+}
+
+// returns true if val is between the values specified by x1 and x2 inclusive
+bool isBetwClosed(val,x1,x2){
+    return (val>=std::min(x1,x2)) && (val<=std::max(x1,x2));
+}
+
+// returns true if val is between the values specified by x1(inclusive) and x2 (exclusive)
+bool isBetwLeftClosed(val,x1,x2){
+    if (x1<x2){
+        return (val>=x1) && (val<x2);
+    }else if(x1>x2){
+        return (val>x2) && (val=<x1);
+    }else{
+        return val==x1;
+    }
 }
 
 // Implement your methods in the `.cpp` file, for example:
@@ -61,22 +77,29 @@ bool MyBugAlgorithm::checkCollision(const amp::Problem2D& problem, Eigen::vector
     return true;
 }
 
+/**
+ * Checks entire workspace for collision
+ * 
+ * @param problem the dataframe that contains the workspace ans obstacles
+ * @param x the 2d point to check
+*/
 bool MyBugAlgorithm::insideObstacles(const amp::Problem2D& problem, Eigen::vector2d x){
-    // predefine some variables
-    Eigen::Vector2d p1;
-    Eigen::Vector2d p2;
-    int num_verts;
-    int num_intersections;
-
     // iterate through obstacles
     for(auto obs : problem){
-
-
+        if insidePolygon(obs,x)
+            return true
     }
     return false;
 }
 
-bool MyBugAlgorithm::insidePolygon(amp::Polygon pg, Eigen::Vector2d q){
+/**
+ * Checks if a point is within a polygon, edge inclusive. 
+ * 
+ * @param pg the polygon to check against
+ * @param q the point to check
+ * @return true if q is inside or on pg, false otherwise
+*/
+bool MyBugAlgorithm::insidePolygon(const amp::Polygon& pg, const Eigen::Vector2d& q){
         //reset number of intersections for each obstacle
     int num_intersections = 0;
 
@@ -88,39 +111,46 @@ bool MyBugAlgorithm::insidePolygon(amp::Polygon pg, Eigen::Vector2d q){
         p1 = obs[i];
         p2 = obs[(i+1)%num_verts]; //gets the next vertex (and wraps to the begining)
 
-
-        // slope of zero (horiz) edge condition
-        if((p2[1]-p1[1])==0){
-            // otherwise horizontal lines dont count as intersection. Do nothing.
-
-        // check the first vertex. If same y (and to the left exclusive), count as intersection
-        }else if(q[1]==p1[1] && q[0]>p1[1]){
-            num_intersections++;        
-
-        // inf slope (vert) edge condition
-        }else if((p2[0]-p1[0])==0){
-            if(p2[0] == q[0]){
-                // point is to the right of the line. Do nothing. 
-
-            }else if(isBetween(q[1],p1[1],p2[1])){
-                // point is to the left and is vertically within other bounds
-                num_intersections++;
-            }
-            continue;
-        }
-
         // calculate the line for the edge
         double m = (p2[1]-p1[1])/(p2[0]-p1[0]);
         double x_inter = (q[1]-p1[1])/m + p[0];
+        double f_of_x = [](std::double x){
+            //lambda function for line
+            if (std::isfinite(m))
+                return (m*(x-p1[0]))+p1[1]; 
+            else 
+                return NAN;
+        }; 
 
         // only checking for intersections on the right of the point
-        if (x_inter<q[0]){
-            //intersection occurs to the left of q[0]. continue.
-            continue;
-        }
+        if (m!=0 && x_inter<q[0]){
+            // intersection occurs to the left of q[0]. 
+            // Pass.
 
-        //
-        if(isBetween(x_inter,p1[0],p2[0])){
+        //check to see if point lies on the edge. Return true if so.
+        }else if(q[1]==f_of_x(q[0]) && isBetwClosed(q[0],p1[0],p2[0])){
+            return true;
+        
+        // slope of zero (horiz) edge condition
+        }else if(m==0){
+            // horizontal lines dont count as intersection. 
+            // Pass.
+
+        // inf slope (vert) edge condition
+        }else if(std::isinf(m) || std::isinf(-m)){
+            if(q[0] <= p1[0] && isBetwLeftClosed(q[1],p1[1],p2[1])){
+                // point is to the left inclusive and is vertically within bounds
+                num_intersections++;
+            }
+
+        // check the first vertex. If same y (and to the left exclusive), count as intersection
+        }else if(q[1]==p1[1] && q[0]>p1[1]){
+            //might be hnadled by the last if statement?
+            num_intersections++;        
+        
+
+        // check if the x intersept is between the two x coords of points
+        }else if(isBetwLeftClosed(x_inter,p1[0],p2[0])){
             num_intersections++;
         }
     }
@@ -128,4 +158,5 @@ bool MyBugAlgorithm::insidePolygon(amp::Polygon pg, Eigen::Vector2d q){
     // odd number of intersections means inside obstacle
     if(num_intersections%2==1)
         return true;
+    return false;
 }
