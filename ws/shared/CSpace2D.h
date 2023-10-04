27d2c62177c2146ad56e593e995e6af326627da1
Luke dt vm <luke.roberson@colorado.edu>
2023-10-04 15:35:18 -0600

    writing code for minkowski diff

---
+++ ws/shared/CSpace2D.h
@@ -0,0 +1,12 @@
+#include "AMPCore.h"
+#include <math.h>
+class CSpace2D : public ConfigurationSpace2D {
+    public:
+
+
+        virtual bool inCollision(double x0, double x1) const override{
+            // TODO
+        }
+
+
+};
