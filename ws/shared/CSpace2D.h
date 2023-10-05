b90e53e7c08ea6b903f779f91c93f1bd3dd16eda
Luke dt vm <luke.roberson@colorado.edu>
2023-10-04 19:11:05 -0600

    monkoski difference stuff runs, but has bug. No seg fault.

--- ws/shared/CSpace2D.h
+++ ws/shared/CSpace2D.h
@@ -1,16 +1,13 @@
 #include "AMPCore.h"
 #include <math.h>
 
-
-
 class CSpace2D : public amp::ConfigurationSpace2D {
     public:
         CSpace2D(double x0_min, double x0_max, double x1_min, double x1_max);
         
-
         amp::Polygon minkDiff(const amp::Polygon& obs,  const amp::Polygon& rob_pos);
         amp::Polygon pgDifference(const amp::Polygon& pg);
-        amp::Polygon zeroPgCCW(const amp::Polygon& pg);
+        amp::Polygon reorderPGCCW(const amp::Polygon& pg, bool zero=false);
         int botLeftVerCCW(const amp::Polygon& pg);
 
         bool inCollision(double x0, double x1) const override{
@@ -19,8 +16,8 @@ class CSpace2D : public amp::ConfigurationSpace2D {
 
         /// @brief the angle of the line (x axis=0, positive in ccw dir) defined by two points 
         inline double ang02pi(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2){
-            double ang = atan2(p1[1]-p2[1],p1[0]-p2[0]);
-            if (ang<0) return ang+360;
+            double ang = atan2(p2[1]-p1[1],p2[0]-p1[0]);
+            if (ang<0){return ang+(2*3.1415);}
             else return ang;
         }
 
