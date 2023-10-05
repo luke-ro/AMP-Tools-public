85aee91c876168af530eec003d583753b7daecc9
Luke dt vm <luke.roberson@colorado.edu>
2023-10-04 18:21:32 -0600

    moved everthing into the class CSpace2D

--- ws/hw4/main.cpp
+++ ws/hw4/main.cpp
@@ -5,15 +5,23 @@
 #include "hw/HW4.h"
 
 // Include the header of the shared class
-#include "HelpfulClass.h"
+#include "CSpace2D.h"
 
-using namespace amp;
+// using namespace amp;
 
 int main(int argc, char** argv) {
     /* Include this line to have different randomized environments every time you run your code (NOTE: this has no affect on grade()) */
     amp::RNG::seed(amp::RNG::randiUnbounded());
 
+    /*** Q1 ***/
+    CSpace2D cspace(-5.0,5.0,-5.0,5.0);
+    amp::Polygon triang = amp::HW4::getEx1TriangleObstacle();
+    amp::Polygon C_triang = cspace.minkDiff(triang,triang);
+    for (auto vert : C_triang.verticesCCW()){
+        std::cout << vert[0]<< ", " << vert[1] << "\n";
+    }
+
     // Grade method
-    //amp::HW4::grade<MyLinkManipulator>(constructor, "nonhuman.biologic@myspace.edu", argc, argv);
+    //amp::HW4::grade<MyLinkManipulator>(constructor, "luke.roberson@colorado.edu", argc, argv);
     return 0;
 }
--- ws/shared/CSpace2D.cpp
+++ ws/shared/CSpace2D.cpp
@@ -1,5 +1,8 @@
 #include "CSpace2D.h"
 
+CSpace2D::CSpace2D(double x0_min, double x0_max, double x1_min, double x1_max)
+ :ConfigurationSpace2D(x0_min,x0_max,x1_min,x1_max){};
+
 /**
  * Takes in a 2D  obstacle and robot and returns the minkowski difference
  * 
@@ -7,36 +10,33 @@
  * @param robot the polygon describing the robot in the workspace
  * @return the polygon of the C-Space obstacle
 */
-amp::Polygon minkDiff(const amp::Polygon& obs,  const amp::Polygon& rob_pos){
+amp::Polygon CSpace2D::minkDiff(const amp::Polygon& obstacle,  const amp::Polygon& rob_pos){
     amp::Polygon rob = pgDifference(rob_pos); 
-    rob = zeroPgCCW(robot_neg);
+    rob = zeroPgCCW(rob);
+    amp::Polygon obs = zeroPgCCW(obstacle);
 
     int i=1;
-    int n=obs.verticesCCW.size();
+    int n=obs.verticesCCW().size();
 
     int j=1;
-    int n=rob.verticesCCW.size();
+    int m=rob.verticesCCW().size();
 
     std::vector<Eigen::Vector2d> verts;
     do{
-        verts.push_back(obs.verticesCCW[i]+rob.verticesCCW[j]);
-        if 
-    }while(!((i==n+1) && (j==m+1)))
+        verts.push_back(obs.verticesCCW()[i]+rob.verticesCCW()[j]);
+        if (ang02pi(obs.verticesCCW()[i%n],obs.verticesCCW()[(i+1)%n]) > ang02pi(rob.verticesCCW()[j%m],rob.verticesCCW()[(j+1)%m])){
+            i++;
+        }else if(ang02pi(obs.verticesCCW()[i%n],obs.verticesCCW()[(i+1)%n]) < ang02pi(rob.verticesCCW()[j%m],rob.verticesCCW()[(j+1)%m])){
+            j++;
+        }else{
+            i++;
+            j++;
+        }
+    }while(!((i==n+1) && (j==m+1)));
+
+    return amp::Polygon(verts);
 }
 
-/**
- * Return the negative of a polygon
- * 
- * @param pg polygon to find the negative of
- * @return the negative of the polygon
-*/
-amp::Polygon pgDifference(const amp::Polygon& pg){
-    std::vector<Eigen::Vector2d> verts;
-    for(int i=pg.verticesCCW.size()-1; i>=0; i--){
-        verts.pushback(-g.verticesCCW[i]);
-    }
-    return amp::Polygon(verts); 
-}
 
 /**
  * Takes in a 2D polygon and returns the index of lowest (and leftmost if tied) vertex
@@ -44,12 +44,12 @@ amp::Polygon pgDifference(const amp::Polygon& pg){
  * @param pg the polygon 
  * @return the index of the lowest (then leftmost if tie) vertex
 */
-int botLeftVerCCW(const amp::Polygon& pg){
+int CSpace2D::botLeftVerCCW(const amp::Polygon& pg){
     int i=0;
-    min_i=-1;
-    Eigen::Vector2d min_ver=pg.verticesCCW[0];
+    int min_i=-1;
+    Eigen::Vector2d min_ver=pg.verticesCCW()[0];
 
-    for(auto const& ver : pg.verticesCCW){
+    for(auto ver : pg.verticesCCW()){
         // Check if y is lowest coord
         if (ver[1]<min_ver[1]){
             min_ver = ver;
@@ -66,25 +66,35 @@ int botLeftVerCCW(const amp::Polygon& pg){
 }
 
 /**
+ * Return the negative of a polygon
+ * 
+ * @param pg polygon to find the negative of
+ * @return the negative of the polygon
+*/
+amp::Polygon CSpace2D::pgDifference(const amp::Polygon& pg){
+    std::vector<Eigen::Vector2d> verts;
+    for(int i=pg.verticesCCW().size()-1; i>=0; i--){
+        verts.push_back(-pg.verticesCCW()[i]);
+    }
+    return amp::Polygon(verts); 
+}
+
+/**
  * puts the lowest left vertex on the origin (0,0)
  * 
  * @param pg a convex polygon
  * @return a new polygon with first vertex as bottom left vertex on origin. 
 */
-amp::Polygon zeroPgCCW(amp::Polygon pg){
+amp::Polygon CSpace2D::zeroPgCCW(const amp::Polygon& pg) { 
     int offset = botLeftVerCCW(pg);
-    Eigen::Vector2d botLeft = pg.verticesCCW[offset];
+    Eigen::Vector2d botLeft = pg.verticesCCW()[offset];
 
     std::vector<Eigen::Vector2d> verts;
-    int sz = pg.verticesCCW.size();
+    int sz = pg.verticesCCW().size();
 
     for(int i=0; i<sz;i++){
-        verts.pushback(pg[(i+offset)%sz]-botLeft)
+        verts.push_back(pg.verticesCCW()[(i+offset)%sz]-botLeft);
     }
 
     return amp::Polygon(verts);
-}
-
-void MyClass::hereIsAMethod() {
-    // Implementation
 }
