4997ec44712a42f7977566f5591784082e403486
Luke dt vm <luke.roberson@colorado.edu>
2023-10-04 19:11:05 -0600

    monkoski difference stuff runs, but has bug. No seg fault.

--- ws/shared/CSpace2D.cpp
+++ ws/shared/CSpace2D.cpp
@@ -4,7 +4,7 @@ CSpace2D::CSpace2D(double x0_min, double x0_max, double x1_min, double x1_max)
  :ConfigurationSpace2D(x0_min,x0_max,x1_min,x1_max){};
 
 /**
- * Takes in a 2D  obstacle and robot and returns the minkowski difference
+ * @brief Takes in a 2D  obstacle and robot and returns the minkowski difference
  * 
  * @param obstacle the polygon describing the obstacle in the workspace
  * @param robot the polygon describing the robot in the workspace
@@ -12,13 +12,13 @@ CSpace2D::CSpace2D(double x0_min, double x0_max, double x1_min, double x1_max)
 */
 amp::Polygon CSpace2D::minkDiff(const amp::Polygon& obstacle,  const amp::Polygon& rob_pos){
     amp::Polygon rob = pgDifference(rob_pos); 
-    rob = zeroPgCCW(rob);
-    amp::Polygon obs = zeroPgCCW(obstacle);
+    rob = reorderPGCCW(rob, false);
+    amp::Polygon obs = reorderPGCCW(obstacle);
 
-    int i=1;
+    int i=0;
     int n=obs.verticesCCW().size();
 
-    int j=1;
+    int j=0;
     int m=rob.verticesCCW().size();
 
     std::vector<Eigen::Vector2d> verts;
@@ -32,21 +32,21 @@ amp::Polygon CSpace2D::minkDiff(const amp::Polygon& obstacle,  const amp::Polygo
             i++;
             j++;
         }
-    }while(!((i==n+1) && (j==m+1)));
+    }while(!((i==n) && (j==m)));
 
     return amp::Polygon(verts);
 }
 
 
 /**
- * Takes in a 2D polygon and returns the index of lowest (and leftmost if tied) vertex
+ * @brief Takes in a 2D polygon and returns the index of lowest (and leftmost if tied) vertex
  * 
  * @param pg the polygon 
  * @return the index of the lowest (then leftmost if tie) vertex
 */
 int CSpace2D::botLeftVerCCW(const amp::Polygon& pg){
     int i=0;
-    int min_i=-1;
+    int min_i=0;
     Eigen::Vector2d min_ver=pg.verticesCCW()[0];
 
     for(auto ver : pg.verticesCCW()){
@@ -66,7 +66,7 @@ int CSpace2D::botLeftVerCCW(const amp::Polygon& pg){
 }
 
 /**
- * Return the negative of a polygon
+ * @brief Return the negative of a polygon
  * 
  * @param pg polygon to find the negative of
  * @return the negative of the polygon
@@ -80,21 +80,28 @@ amp::Polygon CSpace2D::pgDifference(const amp::Polygon& pg){
 }
 
 /**
- * puts the lowest left vertex on the origin (0,0)
+ * @brief puts the lowest left vertex on the origin (0,0)
  * 
  * @param pg a convex polygon
  * @return a new polygon with first vertex as bottom left vertex on origin. 
 */
-amp::Polygon CSpace2D::zeroPgCCW(const amp::Polygon& pg) { 
-    int offset = botLeftVerCCW(pg);
+amp::Polygon CSpace2D::reorderPGCCW(const amp::Polygon& pg, bool zero) { 
+    int offset = botLeftVerCCW(pg); // Fix this func
     Eigen::Vector2d botLeft = pg.verticesCCW()[offset];
 
     std::vector<Eigen::Vector2d> verts;
     int sz = pg.verticesCCW().size();
 
-    for(int i=0; i<sz;i++){
-        verts.push_back(pg.verticesCCW()[(i+offset)%sz]-botLeft);
+    if (zero){
+        for(int i=0; i<sz;i++){
+            verts.push_back(pg.verticesCCW()[(i+offset)%sz]-botLeft);
+        }
+    }else{
+        for(int i=0; i<sz;i++){
+            verts.push_back(pg.verticesCCW()[(i+offset)%sz]);
+        }
     }
+    
 
     return amp::Polygon(verts);
 }
