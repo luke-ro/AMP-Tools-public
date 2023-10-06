b9434d7528b4aa6ce419f720e19824341558e839
Luke dt vm <luke.roberson@colorado.edu>
2023-10-06 13:11:48 -0600

    think I have figure out the polymorphism in construct?

--- ws/shared/Arm2L.cpp
+++ ws/shared/Arm2L.cpp
@@ -1,10 +1,10 @@
 #include "Arm2L.h"
 
 Eigen::Vector2d Arm2L::getJointLocation(const amp::ManipulatorState& state, uint32_t joint_index) const{
-    return;
+    return Eigen::Vector2d();
 }
 
 
 amp::ManipulatorState Arm2L::getConfigurationFromIK(const Eigen::Vector2d& end_effector_location) const{
-    return;
+    return amp::ManipulatorState ();
 }
