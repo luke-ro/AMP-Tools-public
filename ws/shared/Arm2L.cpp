2677401f63a00510621e0124702c46b869467f75
Luke dt vm <luke.roberson@colorado.edu>
2023-10-06 08:42:06 -0600

    link class setup

---
+++ ws/shared/Arm2L.cpp
@@ -0,0 +1,10 @@
+#include "Arm2L.h"
+
+Eigen::Vector2d Arm2L::getJointLocation(const amp::ManipulatorState& state, uint32_t joint_index) const{
+    return;
+}
+
+
+amp::ManipulatorState Arm2L::getConfigurationFromIK(const Eigen::Vector2d& end_effector_location) const{
+    return;
+}
