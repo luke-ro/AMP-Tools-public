835c6aa810bbc6896380666427f22189dd3612e5
Luke dt vm <luke.roberson@colorado.edu>
2023-10-06 13:23:52 -0600

    added foward kin

--- ws/shared/Arm2L.cpp
+++ ws/shared/Arm2L.cpp
@@ -1,7 +1,15 @@
 #include "Arm2L.h"
 
 Eigen::Vector2d Arm2L::getJointLocation(const amp::ManipulatorState& state, uint32_t joint_index) const{
-    return Eigen::Vector2d();
+    Eigen::Vector2d loc = m_base_location;
+
+    // Go through links to get nth joint location
+    for(int i =0; i<joint_index; i++){
+        loc[0] += m_link_lengths[i]*cos(state[i]);
+        loc[1] += m_link_lengths[i]*sin(state[i]);
+    }
+
+    return loc;
 }
 
 
