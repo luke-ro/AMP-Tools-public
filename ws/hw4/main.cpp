99cd7918a4d9315c6cacee44458fbdd4c4b51104
Luke dt vm <luke.roberson@colorado.edu>
2023-10-07 15:24:17 -0600

    fixed foward kinematics

--- ws/hw4/main.cpp
+++ ws/hw4/main.cpp
@@ -79,7 +79,7 @@ int main(int argc, char** argv) {
     /*** 2a ***/
     Eigen::Vector2d base {0,0};
     // amp::ManipulatorState state_2a = {pi/2, pi/2, -pi/3};
-    amp::ManipulatorState state_2a = {pi/6, pi/3+pi/6, 7*pi/4+pi/3+pi/6};
+    amp::ManipulatorState state_2a = {pi/6, pi/3, 7*pi/4};
     std::vector<double> lengths_2a = {0.5, 1.0, 0.5};
     Arm2L manip_2a(base,lengths_2a);
     // amp::HW4::checkFK(Eigen::Vector2d(0,0), 2, manip_2a, state_2a);
@@ -95,19 +95,23 @@ int main(int argc, char** argv) {
     amp::Visualizer::makeFigure(manip_2b, state_2b);
 
 
+
     /*** 3 ***/
-    // amp::ManipulatorState state_2a = {pi/6, pi/3+pi/6, 7*pi/4+pi/3+pi/6};
-    amp::Environment2D env3a = amp::HW4::getEx3Workspace1();
-    // std::vector<double> lengths_2a = {1.0, 1.0};
-    Arm2L manip_3(base, lengths_2b);
-    // std::unique_ptr<amp::GridCSpace2D> ptr_3a = MyGridCon mgc(manip_3, env3a);
-    // auto ptr = std::make_unique<CSpace2D>(env.x_min,env.x_max,env.y_min,env.y_max);
-    // auto ptr_3a = std::make_unique<amp::GridCSpace2D>(manip_3, env3a);
-    CSpace2D gridcon(0, 2*pi, 0, 2*pi, 10, 10);
-    // amp::HW4::checkCSpace()
-    amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace1()));
-    amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace2()));
-    amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace3()));
+    // // amp::ManipulatorState state_2a = {pi/6, pi/3+pi/6, 7*pi/4+pi/3+pi/6};
+    // amp::Environment2D env3a = amp::HW4::getEx3Workspace1();
+    // // std::vector<double> lengths_2a = {1.0, 1.0};
+    // Arm2L manip_3(base, lengths_2b);
+    // // std::unique_ptr<amp::GridCSpace2D> ptr_3a = MyGridCon mgc(manip_3, env3a);
+    // // auto ptr = std::make_unique<CSpace2D>(env.x_min,env.x_max,env.y_min,env.y_max);
+    // // auto ptr_3a = std::make_unique<amp::GridCSpace2D>(manip_3, env3a);
+    // CSpace2D gridcon(0, 2*pi, 0, 2*pi, 100, 100);
+    // // amp::HW4::checkCSpace()
+    // amp::Visualizer::makeFigure(amp::HW4::getEx3Workspace1());
+    // amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace1()));
+    // amp::Visualizer::makeFigure(amp::HW4::getEx3Workspace2());
+    // amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace2()));
+    // amp::Visualizer::makeFigure(amp::HW4::getEx3Workspace3());
+    // amp::Visualizer::makeFigure(gridcon.genCSpace(manip_3,amp::HW4::getEx3Workspace3()));
 
 
     // Grade method
--- ws/shared/Rotate.h
+++ ws/shared/Rotate.h
@@ -1,9 +1,11 @@
+#pragma once
 
 #include "AMPCore.h"
+
 namespace Rotate{
 
 /// @brief the angle of the line (x axis=0, positive in ccw dir) defined by two points 
-inline double ang02pi(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2){
+static double ang02pi(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2){
     double ang = atan2(p2[1]-p1[1],p2[0]-p1[0]);
     if (ang<0){return ang+(2*3.1415);}
     else return ang;
@@ -12,6 +14,99 @@ inline double ang02pi(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2){
 /// @brief the angle of the line (x axis=0, positive in ccw dir) defined by two points 
 inline double ang(const Eigen::Vector2d& p1, const Eigen::Vector2d& p2){
     return atan2(p2[1]-p1[1],p2[0]-p1[0]);
+}
+
+inline void rotateLine2DInPlace(Eigen::Vector2d& p1, Eigen::Vector2d& p2, double angle, const Eigen::Vector2d pivot){
+    Eigen::Matrix3d R;
+    Eigen::Matrix3d T1;
+    Eigen::Matrix3d T2;
+
+    T1 << 1,  0,  -pivot[0],
+          0,  1,  -pivot[1],
+          0,  0,  1;
+
+    R <<  cos(angle),  -sin(angle),  0,
+         sin(angle),  cos(angle),  0,
+          0,           0,           1;
+
+
+    T2 << 1,  0,  pivot[0],
+          0,  1,  pivot[1],
+          0,  0,  1;
+    
+    Eigen::Matrix3d T = T2*R*T1;
+
+    Eigen::Vector3d u = {p1[0], p1[1], 1};
+    Eigen::Vector3d v = {p2[0], p2[1], 1};
+
+    u = T*u;
+    v = T*v;
+
+    p1[0] = u[0];
+    p1[1] = u[1];
+
+    p2[0] = v[0];
+    p2[1] = v[1];
+}
+
+inline Eigen::Vector2d rotatePoint(const Eigen::Vector2d& p1, double angle, const Eigen::Vector2d& pivot){
+    Eigen::Matrix3d R;
+    Eigen::Matrix3d T1;
+    Eigen::Matrix3d T2;
+
+    T1 << 1,  0,  -pivot[0],
+          0,  1,  -pivot[1],
+          0,  0,  1;
+
+    R <<  cos(angle), -sin(angle),  0,
+          sin(angle),  cos(angle),  0,
+          0,           0,           1;
+
+
+    T2 << 1,  0,  pivot[0],
+          0,  1,  pivot[1],
+          0,  0,  1;
+    
+    Eigen::Matrix3d T = T2*R*T1;
+
+    Eigen::Vector3d u = {p1[0], p1[1], 1};
+    u = T*u;
+
+    Eigen::Vector2d temp {u[0], u[1]};
+    return temp;
+}
+
+inline amp::Polygon rotatePG(amp::Polygon pg, double angle, Eigen::Vector2d pivot){
+    Eigen::Matrix3d R;
+    Eigen::Matrix3d T1;
+    Eigen::Matrix3d T2;
+
+    T1 << 1,  0,  -pivot[0],
+          0,  1,  -pivot[1],
+          0,  0,  1;
+
+    R <<  cos(angle), -sin(angle),  0,
+          sin(angle),  cos(angle),  0,
+          0,           0,           1;
+
+
+    T2 << 1,  0,  pivot[0],
+          0,  1,  pivot[1],
+          0,  0,  1;
+    
+    Eigen::Matrix3d T = T2*R*T1;
+    
+    std::vector<Eigen::Vector2d> verts;
+    for(auto vert: pg.verticesCCW()){
+        Eigen::Vector3d u;
+        u << vert[0], vert[1], 1;
+        Eigen::Vector3d v = T*u;
+        Eigen::Vector2d w;
+        w << v[0], v[1];
+        verts.push_back(w);
+    }
+
+    return amp::Polygon(verts);
 }
 
 }
