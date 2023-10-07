67d35881790eee06039140caf75c66d6c681e216
Luke dt vm <luke.roberson@colorado.edu>
2023-10-07 00:09:01 -0600

    turn in state

--- ws/shared/Arm2L.cpp
+++ ws/shared/Arm2L.cpp
@@ -13,7 +13,7 @@ Eigen::Vector2d Arm2L::getJointLocation(const amp::ManipulatorState& state, uint
     Eigen::Vector2d loc = m_base_location;
 
     // Go through links to get nth joint location
-    for(int i =0; i<joint_index; i++){
+    for(int i=0; i<joint_index; i++){
         loc[0] += m_link_lengths[i]*cos(state[i]);
         loc[1] += m_link_lengths[i]*sin(state[i]);
     }
--- ws/shared/MyGridCon.cpp
+++ ws/shared/MyGridCon.cpp
@@ -1,13 +1,13 @@
-#include "MyGridCon.h"
+// #include "MyGridCon.h"
 
-std::unique_ptr<amp::GridCSpace2D> MyGridCon::construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env){
-            // makes a CSpace2D object and initializes it with bounds from env
-            // std::unique_ptr<amp::GridCSpace2D> cspace(new CSpace2D(env.x_min,env.x_max,env.y_min,env.y_max));
+// std::unique_ptr<amp::GridCSpace2D> MyGridCon::construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env){
+//             // makes a CSpace2D object and initializes it with bounds from env
+//             // std::unique_ptr<amp::GridCSpace2D> cspace(new CSpace2D(env.x_min,env.x_max,env.y_min,env.y_max));
             
-            auto ptr = std::make_unique<CSpace2D>(env.x_min,env.x_max,env.y_min,env.y_max);
-            // std::unique_ptr<amp::ConfigurationSpace2D> ptr = &cspace;
-            // cspace->in
-            ptr->genCSpace(manipulator,env); //genCSpace(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env);
-            // std::unique_ptr<amp::ConfigurationSpace2D> n_ptr(ptr);
-            return ptr;
-        }
+//             auto ptr = std::make_unique<CSpace2D>(env.x_min,env.x_max,env.y_min,env.y_max);
+//             // std::unique_ptr<amp::ConfigurationSpace2D> ptr = &cspace;
+//             // cspace->in
+//             ptr->genCSpace(manipulator,env); //genCSpace(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env);
+//             // std::unique_ptr<amp::ConfigurationSpace2D> n_ptr(ptr);
+//             return ptr;
+//         }
