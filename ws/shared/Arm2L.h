1ccc7c2c6512860b4e57da1322bfa762065674b6
Luke dt vm <luke.roberson@colorado.edu>
2023-10-06 18:12:44 -0600

    arm stuff runs but doesn't quite work.

--- ws/shared/Arm2L.h
+++ ws/shared/Arm2L.h
@@ -1,8 +1,13 @@
 #include "AMPCore.h"
+#include "Rotate.h"
 
 class Arm2L : public amp::LinkManipulator2D {
     public: 
-        Arm2L();
+        Arm2L() : LinkManipulator2D() {}
+
+        Arm2L(const std::vector<double>& link_lengths);
+
+        Arm2L(const Eigen::Vector2d& base_location, const std::vector<double>& link_lengths);
 
         Eigen::Vector2d getJointLocation(const amp::ManipulatorState& state, uint32_t joint_index) const override;
         amp::ManipulatorState getConfigurationFromIK(const Eigen::Vector2d& end_effector_location) const override;
