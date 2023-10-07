42dc8ea152905b4c76558a3da4ae61a0566b0eae
Luke dt vm <luke.roberson@colorado.edu>
2023-10-07 00:09:01 -0600

    turn in state

--- ws/shared/MyGridCon.h
+++ ws/shared/MyGridCon.h
@@ -1,9 +1,9 @@
-#include "AMPCore.h"
-#include "CSpace2D.h"
-#include "hw/HW4.h"
+// #include "AMPCore.h"
+// #include "CSpace2D.h"
+// #include "hw/HW4.h"
 
-class MyGridCon : public amp::GridCSpace2DConstructor {
-    public:
+// class MyGridCon : public amp::GridCSpace2DConstructor {
+//     public:
     
-    virtual std::unique_ptr<amp::GridCSpace2D> construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env) override;
-};
+//     virtual std::unique_ptr<amp::GridCSpace2D> construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env) override;
+// };
