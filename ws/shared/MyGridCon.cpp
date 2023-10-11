b2a07c4eec6df4321ebd83eca403186a27443bdd
Luke dt vm <luke.roberson@colorado.edu>
2023-10-11 11:18:31 -0600

    fixed small changes that I made testing

--- ws/shared/MyGridCon.cpp
+++ ws/shared/MyGridCon.cpp
@@ -8,7 +8,7 @@ std::unique_ptr<amp::GridCSpace2D> MyGridCon::construct(const amp::LinkManipulat
             // std::unique_ptr<amp::ConfigurationSpace2D> ptr = &cspace;
             // cspace->in
             CSpace2D temp(env.x_min,env.x_max,env.y_min,env.y_max);
-            ptr = temp.genCSpace(manipulator,env); //genCSpace(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env);
+            // ptr = temp.genCSpace(manipulator,env); //genCSpace(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env);
             // std::unique_ptr<amp::ConfigurationSpace2D> n_ptr(ptr);
             return ptr;
         }
