ad64d7c6716c331ebfb36a04235955fd8e3b3c66
Luke dt vm <luke.roberson@colorado.edu>
2023-10-11 20:11:54 -0600

    worked on function to find closest point on pg

---
+++ ws/hw5/main.cpp
@@ -0,0 +1,10 @@
+// This includes all of the necessary header files in the toolbox
+#include "AMPCore.h"
+
+// Include the correct homework header
+#include "hw/HW5.h"
+
+
+int main(int argc, char** argv) {
+
+}
