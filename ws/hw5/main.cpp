bf9cfbab114f730136c5226d5e9a7550e5515a46
Luke dt vm <luke.roberson@colorado.edu>
2023-10-13 11:44:01 -0600

    made progress I think? Added some random noise to the update step

--- ws/hw5/main.cpp
+++ ws/hw5/main.cpp
@@ -1,6 +1,8 @@
 // This includes all of the necessary header files in the toolbox
 #include "AMPCore.h"
 
+#include "time.h"
+
 // Include the correct homework header
 #include "hw/HW5.h"
 #include "hw/HW2.h"
@@ -8,20 +10,25 @@
 #include "myGDAlgo.h"
 
 int main(int argc, char** argv) {
+
+    // srand(time(NULL));
+    srand(0);
+
     double epsilon=0.1;
     double dstar_goal=5.0;
     double zeta=1.0;
-    double Qstar=1.0;
+    double Qstar=0.5;
     double eta=1.0;
     double alpha=0.1;
     myGDAlgo gd(epsilon, dstar_goal, zeta, Qstar, eta, alpha);
     std::cout<<"here"<<"\n";
 
-    amp::Problem2D prob = amp::HW5::getWorkspace1();
-    // amp::Problem2D prob = amp::HW2::getWorkspace1();
+    amp::Problem2D prob_ez = amp::HW5::getWorkspace1();
+    amp::Problem2D prob = amp::HW2::getWorkspace1();
 
     amp::Path2D path = gd.plan(prob);
 
+    amp::Visualizer::makeFigure(prob_ez,gd.plan(prob_ez));
     amp::Visualizer::makeFigure(prob,path);
     amp::Visualizer::showFigures();
 }
