26ef66f9441aebd5bd77d54e70e0a53536e94087
Luke dt vm <luke.roberson@colorado.edu>
2023-10-13 10:10:07 -0600

    it runs, but it runs through obstacle

--- ws/hw5/main.cpp
+++ ws/hw5/main.cpp
@@ -3,8 +3,25 @@
 
 // Include the correct homework header
 #include "hw/HW5.h"
+#include "hw/HW2.h"
 
+#include "myGDAlgo.h"
 
 int main(int argc, char** argv) {
+    double epsilon=0.1;
+    double dstar_goal=5.0;
+    double zeta=1.0;
+    double Qstar=0.5;
+    double eta=1.0;
+    double alpha=0.1;
+    myGDAlgo gd(epsilon, dstar_goal, zeta, Qstar, eta, alpha);
+    std::cout<<"here"<<"\n";
 
+    // amp::Problem2D prob = amp::HW5::getWorkspace1();
+    amp::Problem2D prob = amp::HW2::getWorkspace1();
+
+    amp::Path2D path = gd.plan(prob);
+
+    amp::Visualizer::makeFigure(prob,path);
+    amp::Visualizer::showFigures();
 }
