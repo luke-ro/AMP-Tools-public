4c8821ebaf37f592b2127c85f39a5c30152b7e2e
Luke dt vm <luke.roberson@colorado.edu>
2023-10-15 15:21:33 -0600

    stuff that I changed before submission

--- ws/hw5/main.cpp
+++ ws/hw5/main.cpp
@@ -8,11 +8,13 @@
 #include "hw/HW2.h"
 
 #include "myGDAlgo.h"
+#include "Helpers.h"
 
 int main(int argc, char** argv) {
 
-    // srand(time(NULL));
-    srand(3);
+    srand(time(NULL));
+    // srand(3);
+
 
     double epsilon=0.1;
     double dstar_goal=3.0;
@@ -26,13 +28,20 @@ int main(int argc, char** argv) {
     amp::Problem2D prob_ez = amp::HW5::getWorkspace1();
     amp::Problem2D prob = amp::HW2::getWorkspace1();
 
+    amp::Path2D path_ez = gd.plan(prob_ez);
+    amp::Visualizer::makeFigure(prob_ez,path_ez);
+    std::cout<<"path_ez length: "<< H::pathDistane(path_ez,0,path_ez.waypoints.size()-1)<<"\n";
+
+    amp::Path2D path_hw2ws2 = gd.plan(amp::HW2::getWorkspace2());
+    std::cout<<"path_hw2ws2 length: "<< H::pathDistane(path_hw2ws2,0,path_hw2ws2.waypoints.size()-1)<<"\n";
+    amp::Visualizer::makeFigure(amp::HW2::getWorkspace2(),path_hw2ws2);
+
     amp::Path2D path = gd.plan(prob);
     amp::HW5::check(path, prob);
-
-    amp::Visualizer::makeFigure(prob_ez,gd.plan(prob_ez));
     amp::Visualizer::makeFigure(prob,path);
+    std::cout<<"path_hw2ws1 length: "<< H::pathDistane(path,0,path.waypoints.size()-1)<<"\n";
 
-    amp::HW5::grade(gd, "luke.roberson@colorado.edu", argc, argv);
+    // amp::HW5::grade(gd, "luke.roberson@colorado.edu", argc, argv);
 
     amp::Visualizer::showFigures();
 }
