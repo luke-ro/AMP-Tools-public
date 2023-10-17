6a52b9f7d1d1c1a19689f5494acdca653ec5d6f3
Luke dt vm <luke.roberson@colorado.edu>
2023-10-16 21:20:52 -0600

    stuff builds, still need to work on constructDisc....

--- ws/hw6/myWaveFront.h
+++ ws/hw6/myWaveFront.h
@@ -6,11 +6,19 @@
 class myWaveFront : public amp::PointWaveFrontAlgorithm{
     public:
 
-    // myWaveFront(){};
+    myWaveFront(){};
 
     //Functions I am overiding
     virtual amp::Path2D planInCSpace(const Eigen::Vector2d& q_init, const Eigen::Vector2d& q_goal, const amp::GridCSpace2D& grid_cspace) override;
     virtual std::unique_ptr<amp::GridCSpace2D> constructDiscretizedWorkspace(const amp::Environment2D& environment) override;
 
+    private:
+
+    int _sz_x0;
+    int _sz_x1;
+    Eigen::Vector2d _x0_bounds;
+    Eigen::Vector2d _x1_bounds;
+
+    // CSpace2D _cspace;
     // ~myWaveFront(){}
 };
