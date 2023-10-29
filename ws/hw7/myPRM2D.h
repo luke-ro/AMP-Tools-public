436ed61523622c1168159fd5fa41e8fdd8d26c4c
Luke dt vm <luke.roberson@colorado.edu>
2023-10-29 11:39:30 -0600

    added parent class for prm and rrt

--- ws/hw7/myPRM2D.h
+++ ws/hw7/myPRM2D.h
@@ -5,32 +5,24 @@
 
 #include "myAStar.h"
 #include "Helpers.h"
+#include "mySamplingBasedMthd.h"
 
-class myPRM2D : public amp::PRM2D{
+class myPRM2D : public mySamplingBasedMthd{
     public:
 
 
 
     myPRM2D(int N=1000, double r=1.0, bool smoothing=true, bool save=false):
-        _N_MAX(N),
-        _neigh_radius(r),
-        _smoothing(smoothing),
-        _save_data(save){}
+        mySamplingBasedMthd(N,r,smoothing,save){}
+
 
     virtual amp::Path2D plan(const amp::Problem2D& problem) override;
 
-    void getData(std::shared_ptr<amp::Graph<double>>& g_ptr, std::map<amp::Node, Eigen::Vector2d>& m);
+
   
 
     private:
 
-    std::vector<Eigen::Vector2d> _node_locs;
-    // amp::Graph<double> _graph;
-    std::shared_ptr<amp::Graph<double>> _graph_ptr;
 
 
-    const int _N_MAX;
-    const double _neigh_radius;
-    const bool _smoothing;
-    bool _save_data;
 };
---
+++ ws/hw7/mySamplingBasedMthd.cpp
@@ -0,0 +1,50 @@
+#include "mySamplingBasedMthd.h"
+
+void mySamplingBasedMthd::getData(std::shared_ptr<amp::Graph<double>>& g_ptr, std::map<amp::Node, Eigen::Vector2d>& m){
+    if(!_save_data){
+        std::cout<<"getData() called but _save_data set to false.\n";
+        g_ptr = _graph_ptr; 
+        m[0] = Eigen::Vector2d();
+        return;
+    }
+    
+    g_ptr = _graph_ptr; 
+
+    {int i=0;
+    for(auto p : _node_locs){
+        m[i] = p;
+    i++;}}
+    return;
+}
+
+
+void mySamplingBasedMthd::smoothPath(const amp::Problem2D& prob, amp::Path2D& path){
+    double dist;
+    int len_start = path.waypoints.size();
+    if(len_start>2){
+        int i,j;
+        for(int k=0; k<len_start*2; k++){
+            int len = path.waypoints.size();
+            // std::cout<<"one iteration\n";
+            i = amp::RNG::randi(0,len);
+            j = amp::RNG::randi(0,len);
+            if(abs(j-i)<=1)
+                continue;
+
+            Eigen::Vector2d p1 = path.waypoints[i];
+            Eigen::Vector2d p2 = path.waypoints[j];
+            dist = (p1-p2).norm();
+            if(H::freeBtwPoints(prob, p1, p2, int(dist*10.0))){
+                //remove nodes between i and j
+                if(j<i){
+                    int temp = i;
+                    i = j;
+                    j = temp;
+                }
+                // std::cout<<"Erasing from ["<<i<<", "<<j<<") len: "<<path.waypoints.size()<<"\n";
+                path.waypoints.erase(path.waypoints.begin()+i+1,path.waypoints.begin()+j);                  ;
+            }
+        }
+    }
+    return;
+}
