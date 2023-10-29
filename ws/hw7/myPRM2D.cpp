187a2393f977140208fe4f9eb640cf0a06ab5b16
Luke dt vm <luke.roberson@colorado.edu>
2023-10-29 11:39:30 -0600

    added parent class for prm and rrt

--- ws/hw7/myPRM2D.cpp
+++ ws/hw7/myPRM2D.cpp
@@ -1,36 +1,5 @@
 #include "myPRM2D.h"
 
-void smoothPath(const amp::Problem2D& prob, amp::Path2D& path){
-    double dist;
-    int len_start = path.waypoints.size();
-    if(len_start>2){
-        int i,j;
-        for(int k=0; k<len_start*2; k++){
-            int len = path.waypoints.size();
-            // std::cout<<"one iteration\n";
-            i = amp::RNG::randi(0,len);
-            j = amp::RNG::randi(0,len);
-            if(abs(j-i)<=1)
-                continue;
-
-            Eigen::Vector2d p1 = path.waypoints[i];
-            Eigen::Vector2d p2 = path.waypoints[j];
-            dist = (p1-p2).norm();
-            if(H::freeBtwPoints(prob, p1, p2, int(dist*10.0))){
-                //remove nodes between i and j
-                if(j<i){
-                    int temp = i;
-                    i = j;
-                    j = temp;
-                }
-                // std::cout<<"Erasing from ["<<i<<", "<<j<<") len: "<<path.waypoints.size()<<"\n";
-                path.waypoints.erase(path.waypoints.begin()+i+1,path.waypoints.begin()+j);                  ;
-            }
-        }
-    }
-    return;
-}
-
 amp::Path2D myPRM2D::plan(const amp::Problem2D& problem){
     // initialize q_init and q_goalS
     amp::ShortestPathProblem spp;
@@ -99,19 +68,3 @@ amp::Path2D myPRM2D::plan(const amp::Problem2D& problem){
 
 
 
-void myPRM2D::getData(std::shared_ptr<amp::Graph<double>>& g_ptr, std::map<amp::Node, Eigen::Vector2d>& m){
-    if(!_save_data){
-        std::cout<<"getPRMData() called but _save_data set to false.\n";
-        g_ptr = _graph_ptr; 
-        m[0] = Eigen::Vector2d();
-        return;
-    }
-    
-    g_ptr = _graph_ptr; 
-
-    {int i=0;
-    for(auto p : _node_locs){
-        m[i] = p;
-    i++;}}
-    return;
-}
