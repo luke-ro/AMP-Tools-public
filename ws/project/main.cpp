9e21f28ddec684e053625aacc8dc1b5e908c5635
lukes ubuntu <luke.roberson@colorado.edu>
2023-12-08 11:25:17 -0500

    made main script. Seg faults

--- ws/project/main.cpp
+++ ws/project/main.cpp
@@ -5,16 +5,66 @@
 #include "Helpers.h"
 #include "QuadAgentProperties.h"
 #include "QuadAgentTools.h"
+#include "QuadMultiRRT.h"
 
+QuadAgentProblem setupSimpleSingleAgentProblem(){
+    QuadAgentProblem prob;
+
+    prob.env = amp::HW4::getEx3Workspace1();
+
+    // setup agent
+    QuadState q_init;
+    q_init << -2,-2,0,0,0,0;
+    QuadState q_goal;
+    q_goal << 2,2,0,0,0,0;
+    QuadAgentProperties agent(q_init,q_goal);
+    
+    prob.num_agents = 1;
+    prob.agents.push_back(agent);
+
+    return prob;
+}
+
+amp::MultiAgentPath2D trajectoriesToAmpMultiAgent(QuadAgentsTrajectories quad_trajs){
+    int n = quad_trajs.size();
+    amp::MultiAgentPath2D amp_paths(n);
+
+    for(int i=0; i<n; i++){
+        amp::Path2D path;
+        for(auto x : quad_trajs[i]){
+            path.waypoints.push_back(Eigen::Vector2d(x(0),x(1)));
+        }
+        amp_paths.agent_paths.push_back(path);
+    }
+    return amp_paths;
+}
+
+amp::MultiAgentProblem2D quadToAmpMultiProblem(QuadAgentProblem quad_prob){
+    amp::MultiAgentProblem2D amp_prob;
+    amp_prob.x_min = quad_prob.env.x_min;
+    amp_prob.x_max = quad_prob.env.x_max;
+    amp_prob.y_min = quad_prob.env.y_min;
+    amp_prob.y_max = quad_prob.env.y_max;
+    amp_prob.obstacles = quad_prob.env.obstacles;
+
+    int num_agents = quad_prob.agents.size();
+    for(int i=0; i<num_agents; i++){
+        amp::CircularAgentProperties amp_agent;
+        amp_agent.radius = quad_prob.agents[i].radius;
+        amp_agent.q_init = QuadAgentTools::getPos(quad_prob.agents[i].q_init);
+        amp_agent.q_goal = QuadAgentTools::getPos(quad_prob.agents[i].q_goal);
+        amp_prob.agent_properties.push_back(amp_agent);
+    }
+
+    return amp_prob;
+}
 
 int main(int argc, char ** argv){
-    QuadState quad_traj;
-    quad_traj<<0,0,0,0,0,0;
+    QuadAgentProblem prob = setupSimpleSingleAgentProblem();
+    QuadMultiRRT kdrrt;
+    QuadAgentsTrajectories quad_trajectories = kdrrt.plan(prob);
+    amp::MultiAgentPath2D amp_paths = trajectoriesToAmpMultiAgent(quad_trajectories);
 
-    QuadAgentProperties agent;
-    Eigen::Vector2d u;
-    QuadDerivativeState test = QuadAgentTools::rk4(agent, quad_traj, u, 0.1);
-    for(int i=0; i<6; i++){
-        std::cout<<test(i)<<"\n";
-    }
+    amp::MultiAgentProblem2D amp_prob = quadToAmpMultiProblem(prob);
+    amp::Visualizer::makeFigure(amp_prob,amp_paths);
 }
