f04ba4bcf0c3022395ac14c10150ef38f61af10a
lukes ubuntu <luke.roberson@colorado.edu>
2023-12-14 10:32:47 -0500

    changed some plotting formats

--- ws/project/plot_plots.py
+++ ws/project/plot_plots.py
@@ -4,15 +4,22 @@ from numpy import genfromtxt
 
 if __name__ == "__main__":
     epsilon_data = genfromtxt(r"/home/user/repos/AMP-Tools-public/testing_output_50_chang_eps.csv",delimiter=",")
+    epsilon_successes = [25,44,33,22]
     # print(epsilon_data)
 
     ## CHanging epsilon for a single agent
-
-    fig,ax = plt.subplots()
+    labels = ["A","B","C","D"]
+    fig,ax = plt.subplots(figsize=(4,3))
     # quants = [(0.25,0.75) for _ in range(4)]
     ax.violinplot(epsilon_data.T, showmeans=False, showmedians=True)
     ax.yaxis.grid(True)
     # ax.set_title("")
     ax.set_ylabel("Time [s]")
-    ax.set_xticks([1,2,3,4],["A","B","C","D"])
+    ax.set_xticks([1,2,3,4],labels)
+
+    fig,ax = plt.subplots(figsize=(4,3))
+    ax.yaxis.grid(True)
+    ax.bar(labels,epsilon_successes,zorder=3)
+    ax.set_xlabel("Test Code",fontsize=15)
+    ax.set_ylabel("Number of Successes")
     plt.show()
